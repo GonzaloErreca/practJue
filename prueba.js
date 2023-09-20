234862f1753a12d280e7a14ba1c0f0a0af0ceb9d
Gonzalo Erreca <gonzaerreca@gmail.com>
2023-09-20 15:02:16 -0300

    modificaciones a debuggear

--- prueba.js
+++ prueba.js
@@ -1,19 +1,19 @@
 //----------------------VARIABLES--------------------------------
 
-const palabraMisteriosa = "ahola"; //RandomWordGenerator();  //carga una palabra random
-let letraAProbar = "";
+const misteryWord = "hola"; //RandomWordGenerator();  genera palabra random
+let testLetter = "";
 let tries = 5;
-let palabraFinal = "";
+let finalWord = "";
 
 //----------------------EJECUCION------------------------------------------------
 
 do {
-  letraAProbar = prompt("Ingrese una letra para adivinar la palabra: ");
+  testLetter = prompt("Ingrese una letra para adivinar la palabra: ");
   let matches = [];
-  if (compararLetraConPalabra()) {
+  if (compareLetterWithWord()) {
     let i = 0;
     while (i !== -1) {
-      let index = palabraMisteriosa.indexOf(letraAProbar, i != 0 ? i+1 : i );
+      let index = misteryWord.indexOf(testLetter, i != 0 ? i+1 : i );
       if (index !== -1 && i!= index) {
         matches.push(index);
       }
@@ -21,27 +21,30 @@ do {
     }
   }
   if (matches.length > 0) {
-    alert("se encontro la letra en la posicion: " + matches);
+    //alert("se encontro la letra en la posicion: " + matches);
+    console.log("se encontro la letra en la posicion: " + matches); // nuevo
   } else {
     tries -= 1;
-    alert( "No se encuentra esa letra, se le resta un intento, le quedan: " + tries
+    console.log( "No se encuentra esa letra, se le resta un intento, le quedan: " + tries
     );
+    //alert( "No se encuentra esa letra, se le resta un intento, le quedan: " + tries );
   }
-} while (tries > 0 && todasLasLetrasEnPalabraM());
+} while (tries > 0 && allCharsOnMisteryWord());
 
-if (todasLasLetrasEnPalabraM() && tries >= 0) {
-  alert("Has adivinado!, la palabra secreta era: " + palabraMisteriosa);
+if (allCharsOnMisteryWord() && tries >= 0) {
+  console.log( "Has adivinado!, la palabra secreta era: " + misteryWord);
+  //alert("Has adivinado!, la palabra secreta era: " + misteryWord);
 } else {
-  alert(
-    "Te has quedado sin tries, la palabra secreta era: " + palabraMisteriosa
-  );
+  console.log("Te has quedado sin tries, la palabra secreta era: " + misteryWord)
+  /* alert( "Te has quedado sin tries, la palabra secreta era: " + misteryWord
+  ); */
 }
 
 //-----------------------------FUNCIONES-----------------------------
 
-function todasLasLetrasEnPalabraM() {
-  for (var i = 0; i < palabraMisteriosa.length; i++) {
-    if (palabraFinal.includes(palabraMisteriosa[i])) {
+function allCharsOnMisteryWord() {
+  for (var i = 0; i < misteryWord.length; i++) {
+    if (finalWord.includes(misteryWord[i])) {
       return false; // Si encuentra una letra que falta, devuelve false
     }
   }
@@ -51,7 +54,7 @@ function todasLasLetrasEnPalabraM() {
 /*
 //a mejorar
 
-function todasLasLetrasEnPalabraM(palabraOriginal, letrasIngresadas) {
+function allCharsOnMisteryWord(palabraOriginal, letrasIngresadas) {
   let palabraCompleta = false;
 
   for (var i = 0; i < palabraOriginal.length; i++) {
@@ -67,14 +70,18 @@ function todasLasLetrasEnPalabraM(palabraOriginal, letrasIngresadas) {
 
 */
 
-function compararLetraConPalabra() {
-  let result = palabraMisteriosa.includes(letraAProbar);
+function compareLetterWithWord() {
+
+  return misteryWord.includes(testLetter); // no seria al reves? testLetter.includes(misteryWord) ????
+
+  //se puede mejorar
+    /* let result = misteryWord.includes(testLetter);
 
   if (result) {
     return result;
   } else {
     return false;
-  }
+  } */
 }
 
 function RandomWordGenerator() {
