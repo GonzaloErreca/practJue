23548894b964aae5b1e4fe603d8ccf58bfbe6761
Gonzalo Erreca <gonzaerreca@gmail.com>
2023-10-19 17:44:04 -0300

    randomizador de cartas

--- MemoryGame/memoryGame.js
+++ MemoryGame/memoryGame.js
@@ -3,51 +3,51 @@ document.addEventListener("DOMContentLoaded", () => {
   const cardArray = [
     {
       name: "leaves",
-      img: "/MemoryGame/IMG/leaves.png",
+      img: "/MemoryGame/Img/leaves.png",
     },
     {
       name: "leaves",
-      img: "/MemoryGame/IMG/leaves.png",
+      img: "/MemoryGame/Img/leaves.png",
     },
     {
       name: "headphones",
-      img: "/MemoryGame/IMG/headphones.png",
+      img: "/MemoryGame/Img/headphones.png",
     },
     {
       name: "headphones",
-      img: "/MemoryGame/IMG/headphones.png",
+      img: "/MemoryGame/Img/headphones.png",
     },
     {
       name: "dices",
-      img: "/MemoryGame/IMG/dices.png",
+      img: "/MemoryGame/Img/dices.png",
     },
     {
       name: "dices",
-      img: "/MemoryGame/IMG/dices.png",
+      img: "/MemoryGame/Img/dices.png",
     },
     {
       name: "memory",
-      img: "/MemoryGame/IMG/memory.png",
+      img: "/MemoryGame/Img/memory.png",
     },
     {
       name: "memory",
-      img: "/MemoryGame/IMG/memory.png",
+      img: "/MemoryGame/Img/memory.png",
     },
     {
       name: "clock",
-      img: "/MemoryGame/IMG/clock.png",
+      img: "/MemoryGame/Img/clock.png",
     },
     {
       name: "clock",
-      img: "/MemoryGame/IMG/clock.png",
+      img: "/MemoryGame/Img/clock.png",
     },
     {
       name: "cherries",
-      img: "/MemoryGame/IMG/cherries.png",
+      img: "/MemoryGame/Img/cherries.png",
     },
     {
       name: "cherries",
-      img: "/MemoryGame/IMG/cherries.png",
+      img: "/MemoryGame/Img/cherries.png",
     }
   ];
 
@@ -59,11 +59,14 @@ var cardsChosenId = [];
 var cardsWon = [];
 
 // crear tablero
+cardArray.sort(() => 0.5 - Math.random());
+
+//console.log(cardArray); //probamos que las cartas esten randomized
 
 function crearTablero() {
   for (let i = 0; i < cardArray.length; i++) {
     let card = document.createElement("img");
-    card.setAttribute("src", "/MemoryGame/IMG/blank.png");
+    card.setAttribute("src", "/MemoryGame/Img/blank.png");
     card.setAttribute("data-id", i);
    // card.addEventListener("click",flipcard)
     grid.appendChild(card);
@@ -79,12 +82,12 @@ function checkForMatch() {
   const optionTwoId = cardsChosenId[1];
   if (cardsChosen[0] === cardsChosen[1]) {
     alert("Encontraste un par!");
-    cards[optionOneId].setAttribute("src", "/MemoryGame/IMG/white.png");
-    cards[optionTwoId].setAttribute("src", "/MemoryGame/IMG/white.png");
+    cards[optionOneId].setAttribute("src", "/MemoryGame/Img/white.png");
+    cards[optionTwoId].setAttribute("src", "/MemoryGame/Img/white.png");
     cardsWon.push(cardsChosen);
   } else {
-    cards[optionOneId].setAttribute("src", "/MemoryGame/IMG/blank");
-    cards[optionTwoId].setAttribute("src", "/MemoryGame/IMG/blank");
+    cards[optionOneId].setAttribute("src", "/MemoryGame/Img/blank");
+    cards[optionTwoId].setAttribute("src", "/MemoryGame/Img/blank");
     alert("Intentalo otra vez!");
   }
 
